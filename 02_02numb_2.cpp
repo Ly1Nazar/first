c2712ec4baf9fd3aa8d0d339eeeaae7611ed2bb2
Ly1Nazar <48887272+Ly1Nazar@users.noreply.github.com>
2020-02-03 11:27:09 +0200

    Add files via upload
    dz 4 (2,15)

---
+++ 02_02numb_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;    
+int main() {
+    int numb,next,count;
+    numb = 0;
+    count = 0;
+    do {
+        printf("enter value of number\n");
+        scanf_s("%d", &next);
+        if ((numb > 0 and next < 0) || (numb < 0 and next>0) ){
+            count++;
+        }
+        numb = next;
+    } while (next != 0);
+    printf("sign changes %d times", count);
+}
